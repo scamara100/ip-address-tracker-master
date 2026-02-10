541ee2969ec408042eb437a1d111685bc6c1dd13
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-02-09 23:34:11 -0500

    add some style on infos labels

--- js/api.js
+++ js/api.js
@@ -1,7 +1,9 @@
 export async function fetchIPData(ip) {
     try {
         const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_W36tmoSFI3ZIFoeY5yqPi7RCkbNYo&ipAddress=${ip}`) // return a promise
+        if (!response.ok) throw new Error("API request failed");
         const data = await response.json() // format data to json
+
         return data
     } catch (e) {
         console.error(e)
@@ -10,7 +12,8 @@ export async function fetchIPData(ip) {
 
 export async function getUserIP() {
     try {
-        const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_W36tmoSFI3ZIFoeY5yqPi7RCkbNYo&ipAddress=${ip}`)
+        const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_W36tmoSFI3ZIFoeY5yqPi7RCkbNYo`)
+        if (!res.ok) throw new Error("API request failed")
         const data = await res.json()
         return data.ip
     } catch (err) {
